e2a59fe52e34238bfbcc4a318a53e945229a1c29
Peter Mensah <ing.mensah@outlook.com>
2026-03-22 12:13:43 +0000

    widget integrated

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import HeroSection from './components/HeroSection'
 import NavBar from './components/NavBar'
 import Products from './components/Products'
 import ClientShowcase from './components/ShowcaseSection'
+import BoafoWidgetInitializer from '../app/components/BoafoWidgetInitializer'
 
 function App() {
   return (
     <div >
+    <BoafoWidgetInitializer />
     <NavBar />
     <HeroSection />
     <AboutProducts />
